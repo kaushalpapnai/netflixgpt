f4541517cbab1eb7b0b67bffc506a8eb59c1074f
Kaushal <kaushal@Kaushals-MacBook-Air.local>
2024-10-16 06:52:01 +0530

    added validate function in form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "./Validate";
 
 const Login = () => {
-   const [isSignInForm,setIsSignInForm] = useState(true)
-   function handleSignIn(){
-    setIsSignInForm(!isSignInForm)
-   }
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const [messsage,setMessage] = useState(null)
+  const email = useRef(null) // we are using useRef method to reference to the email which is typed in input field we can use useState also and pass that in validat.js but useRef is easy
+  const password = useRef(null)
+
+  function toggleSignIn() {
+    setIsSignInForm(!isSignInForm);
+  }
+
+  function handleSignIn(){
+      const res =  checkValidData(email.current.value,password.current.value)    // we are passing email.current.value because useRef gives us object so we have to navigate inside it 
+      setMessage(res)
+  }
+
   return (
     <div>
       <Header />
@@ -15,15 +26,41 @@ const Login = () => {
           alt="background-img"
         />
       </div>
-      <form className="absolute p-12 bg-black mx-auto my-36 right-0 left-0 w-3/12 bg-opacity-80 rounded-lg">
-        <h1 className="font-bold text-3xl py-4 text-white w-full">{isSignInForm?"Sign in":"Sign up"}</h1>
-        {!isSignInForm && <input type="text" placeholder="Full Name" className="p-4 my-4 w-full bg-gray-700" />}
-        <input type="text" placeholder="email address" className="p-4 my-4 w-full bg-gray-700" />
-        <input type="text" placeholder="password" className="p-4 my-4 w-full bg-gray-700" />
-        <button className="p-4 my-6 bg-red-700 w-full rounded-lg text-white">{isSignInForm?"Sign in":"Sign up"}</button>
-         <p className="text-white cursor-pointer" onClick={()=> handleSignIn()}>
-         {isSignInForm?"New to Netflix? Sign Up Now":"Already registered? Sign In Now"}
-         </p>
+      <form onSubmit={(e)=> e.preventDefault()} className="absolute p-12 bg-black mx-auto my-36 right-0 left-0 w-3/12 bg-opacity-80 rounded-lg">
+        <h1 className="font-bold text-3xl py-4 text-white w-full">
+          {isSignInForm ? "Sign in" : "Sign up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700 text-white"
+          />
+        )}
+        <input
+          ref={email} // refference useRef
+          type="text"
+          placeholder="email address"
+          className="p-4 my-4 w-full bg-gray-700 text-white"
+        />
+        <p className="text-red-500">{messsage?.email?messsage?.email:null}</p>
+        <input
+          ref={password} // refference useRef
+          type="text"
+          placeholder="password"
+          className="p-4 my-4 w-full bg-gray-700 text-white"
+        />
+        <p className="text-red-500">{messsage?.password?messsage?.password:null}</p>
+        <button className="p-4 my-6 bg-red-700 w-full rounded-lg text-white"
+           onClick={()=>handleSignIn()}
+        >
+          {isSignInForm ? "Sign in" : "Sign up"}
+        </button>
+        <p className="text-white cursor-pointer" onClick={() => toggleSignIn()}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already registered? Sign In Now"}
+        </p>
       </form>
     </div>
   );
---
+++ src/components/Validate.js
@@ -0,0 +1,31 @@
+// rejex for email validation can find on internet  ---->  /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/
+
+export const checkValidData = (email,password)=>{
+      const isEmailValid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)
+      const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password)
+
+      console.log("email :",email)
+      console.log("password :",password)
+      
+      if(!isEmailValid && !isPasswordValid){
+        const messsage = {
+            email : "email is not valid",
+            password : "password is not valid"
+        }
+        return messsage
+      }
+      
+      if(!isEmailValid){
+        return {
+            email : "email is not valid"
+        }
+      }
+
+      if(!isPasswordValid){
+        return {
+            password : "password is not valid"
+        }
+      }
+
+      return null
+}
